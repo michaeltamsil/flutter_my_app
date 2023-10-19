02a741fdaba41bf3ae2eb948ddcc9c519142b2d9
Michael Tamsil <michaeltamsil@gmaill.com>
2023-10-19 10:47:00 +0700

    48. introducting stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,11 @@ import 'package:my_app/gradient_container.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       home: Scaffold(
         body: GradientContainer(
-          const Color.fromARGB(255, 147, 108, 255),
-          const Color.fromARGB(255, 34, 0, 92),
+          Color.fromARGB(255, 147, 108, 255),
+          Color.fromARGB(255, 34, 0, 92),
         ),
       ),
     ),
